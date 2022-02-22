636b0decdcdfb894526b832ea84030a1c0c1e75d
Kia,Conaty Carley 20088426 <20088426@wit.ie>
2022-02-22 10:25:39 +0000

    Added filtering of movie list.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";  
 import Header from "../components/headerMovieList";
 import FilterCard from "../components/filterMoviesCard";
 import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid";
 
+
 const MovieListPage = (props) => {
-  const movies = props.movies;
+  const [movies, setMovies] = useState([]);
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, []);
 
   return (
     <Grid container sx={{ padding: "20px" }}>
@@ -14,9 +46,13 @@ const MovieListPage = (props) => {
       </Grid>
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <FilterCard />
+        <FilterCard
+      onUserInput={handleChange}
+      titleFilter={nameFilter}
+      genreFilter={genreFilter}
+    />
         </Grid>
-        <MovieList movies={movies}></MovieList>
+        <MovieList movies={displayedMovies} />
       </Grid>
     </Grid>
   );
