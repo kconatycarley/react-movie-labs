cd1cdadecd32643f915813b4aae19ebdaa629bd3
Kia,Conaty Carley 20088426 <20088426@wit.ie>
2022-03-16 17:59:41 +0000

    Bug Fix - make action icons on movie card configurable.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -3,6 +3,7 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const HomePage = (props) => {
 
@@ -21,13 +22,17 @@ const HomePage = (props) => {
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
   const addToFavorites = (movieId) => true 
-
-  return (
-    <PageTemplate
-      title='Discover Movies'
-      movies={movies}
-      selectFavorite={addToFavorites}
-    />
-  );
-};
+  const HomePage = (props) => {
+ 
+    
+    return (
+       <PageTemplate
+         title="Discover Movies"
+         movies={movies}
+         action={(movie) => {
+           return <AddToFavoritesIcon movie={movie} />
+         }}
+       />
+   );
+ };
 export default HomePage;
